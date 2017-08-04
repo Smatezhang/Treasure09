670e457fefcd379cb0778099c1f417cb49122676
jthug <1649369473@qq.com>
2017-08-04 14:08:35 +0800

    百度地图的初始化

--- app/src/main/java/com/example/machenike/treasure9/map/MapFragment.java
+++ app/src/main/java/com/example/machenike/treasure9/map/MapFragment.java
@@ -3,21 +3,103 @@ package com.example.machenike.treasure9.map;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.baidu.mapapi.map.BaiduMapOptions;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapView;
 import com.example.machenike.treasure9.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by MACHENIKE on 2017/8/3.
  */
 
-public class MapFragment extends Fragment{
+public class MapFragment extends Fragment {
+    @BindView(R.id.iv_located)
+    ImageView mIvLocated;
+    @BindView(R.id.btn_HideHere)
+    Button mBtnHideHere;
+    @BindView(R.id.centerLayout)
+    RelativeLayout mCenterLayout;
+    @BindView(R.id.iv_scaleUp)
+    ImageView mIvScaleUp;
+    @BindView(R.id.iv_scaleDown)
+    ImageView mIvScaleDown;
+    @BindView(R.id.tv_located)
+    TextView mTvLocated;
+    @BindView(R.id.tv_satellite)
+    TextView mTvSatellite;
+    @BindView(R.id.tv_compass)
+    TextView mTvCompass;
+    @BindView(R.id.ll_locationBar)
+    LinearLayout mLlLocationBar;
+    @BindView(R.id.tv_currentLocation)
+    TextView mTvCurrentLocation;
+    @BindView(R.id.iv_toTreasureInfo)
+    ImageView mIvToTreasureInfo;
+    @BindView(R.id.et_treasureTitle)
+    EditText mEtTreasureTitle;
+    @BindView(R.id.cardView)
+    CardView mCardView;
+    @BindView(R.id.layout_bottom)
+    FrameLayout mLayoutBottom;
+    @BindView(R.id.map_frame)
+    FrameLayout mMapFrame;
+    Unbinder unbinder;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, null);
+
         return view;
+    }
+
+    //当onCreateView执行完毕之后执行
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        unbinder = ButterKnife.bind(this, view);
+        initView();
+    }
+
+    private void initView() {
+
+        MapStatus mapStatus = new MapStatus.Builder()
+                .overlook(0f)
+                .rotate(0f)
+                .zoom(11)
+                .build();
+
+        BaiduMapOptions options = new BaiduMapOptions();
+        options.mapStatus(mapStatus);
+        options.scaleControlEnabled(false); //隐藏比例尺
+        options.zoomControlsEnabled(false); //隐藏缩放控件
+        options.zoomGesturesEnabled(true);  //允许手势缩放
+        //创建MapView对象
+        MapView mapView = new MapView(getContext(), options);
+
+        //将MapView填充到FrameLayout
+        mMapFrame.addView(mapView,0);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
